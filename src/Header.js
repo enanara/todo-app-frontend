fbd311b506220ea1a97fa12edd9f375ee675a8c4
Enkhnaran <enaa.mongolia@gmail.com>
2019-12-09 14:10:13 +0000

    header added

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Header extends React.Component {
+    render () {
+        return (
+            <div className="row">
+                <h3 className="col-8">
+                    <div className="d-none d-sm-none d-md-block">
+                        <span>Good Morning </span>
+                        <span>Ena</span>
+                    </div>
+                </h3>
+                <div className="col-4">
+                    <date>Thursday </date>
+                    <span className="weather">
+                        9°C 
+                    </span>
+                    <span className="weatherIcon">
+                        &#x1F324;
+                    </span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
